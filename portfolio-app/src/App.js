8032c1e926839a68b94378a3d1ec3c7ae70d74a6
Grant Sobkowski <grant.sobkowski@gmail.com>
2022-02-23 21:42:14 -0500

    init

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+
+      <section className="section intro"></section>
+        {/* <h2 className="sub-heading">Intro</h2> */}
+
+      <section className="section what"></section>
+        {/* <h2 className="sub-heading">What I Do</h2> */}
+
+      <section className="section portfolio"></section>
+        {/* <h2 className="sub-heading">Portfolio</h2> */}
+
+      <section className="section who"></section>
+        {/* <h2 className="sub-heading">More About Me</h2> */}
+
+      <section className="section contact"></section>
+        {/* <h2 className="sub-heading">Contact</h2> */}
+    </div>
+  );
+}
+
+export default App;
